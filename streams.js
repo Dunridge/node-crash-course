b08cd8880137f3560b92bd01c882a1812d500ada
Dunridge <Polinka7max@gmail.com>
2022-08-25 18:32:32 +0300

    write-stream. covered write streams

--- streams.js
+++ streams.js
@@ -2,13 +2,18 @@ const fs = require('fs');
 
 // read streams 
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf-8' });
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
 // .on() - is an event listener 
 // reads a chunk of data from a stream 
-readStream.on('data', (chunk) => {
-    console.log('--- NEW CHUNK ---')
-    console.log(chunk);
-    // console.log(chunk.toString());
-})
+// readStream.on('data', (chunk) => {
+//     console.log('--- NEW CHUNK ---')
+//     console.log(chunk);
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+// })
 
 // write streams
+
+// piping
+readStream.pipe(writeStream);
